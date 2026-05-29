558161d3bc319caaadd889570fa95de9e82bf7ed
invitacionesweb <invitacionesweb.info@gmail.com>
2026-05-29 16:40:02 -0300

    ACTUALIZAR
    AC

---
+++ config.js
@@ -0,0 +1,39 @@
+// =============================================
+//  CONFIGURACIÓN DE LA INVITACIÓN - ROMA
+//  Editá solo este archivo para cada cliente
+// =============================================
+
+const CONFIG = {
+
+  // --- DATOS DEL EVENTO ---
+  nombreFestejado: "Lazaro",
+  subtitulo: "Te esperamos",
+
+  // --- FECHA PARA EL CONTADOR (ISO 8601) ---
+  fechaContador: "2026-06-14T23:59:59",
+
+  // --- UBICACIÓN ---
+  textoUbicacion: "Te espero para celebrar un cumple mundial. Desde las 14 hasta las 16:30 hs, en Club house - Alma verde.",
+  linkMapa: "https://www.google.com/maps/dir//Alma+Verde,+Av.+Hip%C3%B3lito+Irigoyen+1335,+B2900+San+Nicol%C3%A1s+de+Los+Arroyos,+Provincia+de+Buenos+Aires/@-33.3769617,-60.2111392,17z/data=!4m17!1m7!3m6!1s0x95b75df9a34ae451:0xb5141d5da235bbc0!2sAlma+Verde!8m2!3d-33.3769617!4d-60.2085643!16s%2Fg%2F11bwq6q07w!4m8!1m0!1m5!1m1!1s0x95b75df9a34ae451:0xb5141d5da235bbc0!2m2!1d-60.2085643!2d-33.3769617!3e0?entry=ttu&g_ep=EgoyMDI2MDUyNy4wIKXMDSoASAFQAw%3D%3D",
+  linkAgendar: "",
+
+  // --- FORMULARIO ---
+  textoForm: "Tu presencia es lo más importante",
+  textoFormSub: "VENI CÓMODO Y PREPARADO PARA METER MUCHOS GOLES. NO OLVIDES CONFIRMAR ASITENCIA ANTES DEL 10 DE JUNIO",
+  maxInvitados: 5,
+  scriptFormUrl: "https://script.google.com/macros/s/AKfycbzGO9-pZizlA5PNOcpAsl-FB06Ja4QVMWF5G8DxqvhZidZeBM_-FzrdSQraoIWhN6fUfQ/exec",
+
+  // --- AUDIO ---
+  archivoAudio: "./abcd.mp3",
+
+  // --- COLORES ---
+  colores: {
+    color0: "#ffffff",
+    color1: "#1f2a7b",
+    color2: "#f8f3ed",
+    color3: "#CD0D08",
+  },
+
+  // --- TÍTULO DE LA PESTAÑA ---
+  tituloTab: "Lazaro",
+};
