57e1c24a85eed81aa1437007ca1604942ab5165a
invitacionesweb <invitacionesweb.info@gmail.com>
2026-05-29 17:02:27 -0300

    a
    a

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   fechaContador: "2026-06-14T23:59:59",
 
   // --- UBICACIÓN ---
-  textoUbicacion: "Te espero para celebrar un cumple mundial. Desde las 14 hasta las 16:30 hs, en Club house - Alma verde.",
+  textoUbicacion: "Te espero para celebrar un cumple mundial. Desde las 14:00 hasta las 16:30 hs, en Club house - Alma verde.",
   linkMapa: "https://www.google.com/maps/dir//Alma+Verde,+Av.+Hip%C3%B3lito+Irigoyen+1335,+B2900+San+Nicol%C3%A1s+de+Los+Arroyos,+Provincia+de+Buenos+Aires/@-33.3769617,-60.2111392,17z/data=!4m17!1m7!3m6!1s0x95b75df9a34ae451:0xb5141d5da235bbc0!2sAlma+Verde!8m2!3d-33.3769617!4d-60.2085643!16s%2Fg%2F11bwq6q07w!4m8!1m0!1m5!1m1!1s0x95b75df9a34ae451:0xb5141d5da235bbc0!2m2!1d-60.2085643!2d-33.3769617!3e0?entry=ttu&g_ep=EgoyMDI2MDUyNy4wIKXMDSoASAFQAw%3D%3D",
   linkAgendar: "",
 
